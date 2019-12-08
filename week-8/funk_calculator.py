ef43eb08d32368b303bb406cacbf6f3dbb0d967a
Karie Funk <kfunk@my365.bellevue.edu>
2019-12-08 11:13:49 -0700

    update

---
+++ week-8/funk_calculator.py
@@ -0,0 +1,23 @@
+# ---------------------------------------
+# Author: Karie Funk
+# Title: Python Calculator
+# Date:  12/08/2019
+# Description: Math functions in Python.
+# ---------------------------------------
+
+# Create an “add” function with two parameters
+def add(num1, num2):
+	return num1 + num2
+
+# Create a “subtract” function with two parameters
+def subtract(num1, num2):
+	return num1 - num2
+
+# Create a “divide” function with two parameters 
+def divide(num1, num2):
+	return num1 / num2
+
+# Call each function with a separate print statement 
+print(add(357, 24))
+print(subtract(400, 200))
+print(divide(600, 3))
